dbe58ef3e826062f42de03a8c3e752217ce8325e
Adole Samuel Sani <adolesamuel4@gmail.com>
2025-01-22 17:30:02 +0100

    Added Triangle to Shape interface

--- shapes/shapes.go
+++ shapes/shapes.go
@@ -11,6 +11,11 @@ type Circle struct {
 	Radius float64
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Height + rectangle.Width)
 }
@@ -20,6 +25,10 @@ func (rectangle Rectangle) Area() float64 {
 }
 func (circle Circle) Area() float64 {
 	return math.Pi * math.Pow(circle.Radius, 2)
+}
+
+func (triangle Triangle) Area() float64 {
+	return 0.5 * triangle.Base * triangle.Height
 }
 
 type Shape interface {
